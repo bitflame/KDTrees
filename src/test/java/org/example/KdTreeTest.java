371e8d83593f7a9618147347409e7c17aecd94d6
shahin ansari <sansari1@cox.net>
2021-06-25 09:29:47 -0400

    added a parameterized test instance

--- src/test/java/org/example/KdTreeTest.java
+++ src/test/java/org/example/KdTreeTest.java
@@ -1,19 +1,33 @@
 package org.example;
 
 import edu.princeton.cs.algs4.Point2D;
+import edu.princeton.cs.algs4.StdRandom;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.aggregator.ArgumentsAccessor;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
 
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class KdTreeTest {
     private final KdTree kt = new KdTree();
-    Point2D p = new Point2D(.5, .5);
+    Point2D p1 = new Point2D(.5, .5);
+    Point2D p2 = new Point2D(.5, .5);
+    Point2D p3 = new Point2D(.5, .5);
+    Point2D p4 = new Point2D(.5, .5);
+    Point2D p5 = new Point2D(.5, .5);
+    Point2D p6 = new Point2D(.5, .5);
+    Point2D p7 = new Point2D(.5, .5);
+    Point2D p8 = new Point2D(.5, .5);
+    Point2D p9 = new Point2D(.5, .5);
+    Point2D p10 = new Point2D(.5, .5);
+
     @BeforeEach
     void setUp() {
-        kt.insert(p);
     }
 
     @AfterEach
@@ -32,14 +46,13 @@ class KdTreeTest {
     void range() {
     }
 
-    @Test
-    void contains() {
-                assertAll("KdTree",
-                ()-> {boolean result = kt.contains(p);
-                assertEquals(result,true);
-                assertTrue(result);
-        });
-        assertEquals(kt.contains(p),true);
+    @ParameterizedTest
+    @CsvSource({"0.5,0.25", "0.0,0.0", "0.5,0.0", "0.5,0.0", "0.25,0.0","0.0,1.0","1.0,0.5",
+    "1.0,0.5","0.25,0.0","0.0,0.25","0.25,0.0","0.25,0.5","0.75,0.75"})
+    void contains(ArgumentsAccessor argumentsAccessor) {
+        Point2D p = new Point2D(argumentsAccessor.getDouble(0), argumentsAccessor.getDouble(1));
+        kt.insert(p);
+        assertTrue(kt.contains(p));
     }
 
     @Test
