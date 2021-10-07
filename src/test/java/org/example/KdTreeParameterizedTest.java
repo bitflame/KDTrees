5fd3a4db6745502c5e390a90394fd528c12684d3
shahin ansari <sansari1@cox.net>
2021-10-07 08:39:16 -0400

    Parameterized Test using junit5 works also

--- src/test/java/org/example/KdTreeParameterizedTest.java
+++ src/test/java/org/example/KdTreeParameterizedTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.*;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Scanner;
 import java.util.stream.Stream;
 
@@ -36,22 +38,48 @@ class KdTreeParameterizedTest {
             int instanceDataLength = 300;
             Object[] instanceData = new Object[instanceDataLength];
             for (final File fileEntry : folder.listFiles()) {
+                Point2D[] expectedPoints = new Point2D[10];
                 String fileName = fileEntry.getName().toUpperCase();
                 if (fileName.endsWith(".TXT")) {
                     Scanner scanner = new Scanner(fileEntry);
-                    kt = new KdTree();
-                    while (scanner.hasNext()) {
-                        double x = scanner.nextDouble();
+                    KdTree kt = new KdTree();
+                    RectHV r = null;
+                    // read the kdtree points coordinates
+                    String s = "";
+
+                    while (scanner.hasNext() && (!(s = scanner.next()).equals("|"))) {
+                        double x = Double.parseDouble(s);
                         double y = scanner.nextDouble();
                         Point2D p = new Point2D(x, y);
                         kt.insert(p);
                     }
+                    // read the rectangle coordinates
+                    // scanner.skip("|");
+                    s = "";
+                    while (scanner.hasNext() && (!(s = scanner.next()).equals("|"))) {
+                        double xmin = Double.parseDouble(s);
+                        double ymin = scanner.nextDouble();
+                        double xmax = scanner.nextDouble();
+                        double ymax = scanner.nextDouble();
+                        r = new RectHV(xmin, ymin, xmax, ymax);
+                    }
+                    // scanner.skip("|");
+                    int j = 0;
+                    while (scanner.hasNext()) {
+                        double x = scanner.nextDouble();
+                        double y = scanner.nextDouble();
+                        Point2D p = new Point2D(x, y);
+                        expectedPoints[j++] = p;
+                        if (j > (expectedPoints.length / 2)) resizeArray((2 * expectedPoints.length),
+                                expectedPoints);
+                    }
+                    if (i > (instanceData.length / 2))
+                        instanceData = resizeArray((2 * instanceData.length), instanceData);
+                    instanceData[i++] = fileEntry.getName();
                     instanceData[i++] = kt;
                     instanceData[i++] = r;
                     instanceData[i++] = expectedPoints;
-                    if (i > instanceDataLength) instanceData = resizeArray(2 * 3, instanceData);
                 }
-                // System.out.println("Done with file " + fileName + " value of i is: " + i);
             }
             return Stream.of(Arguments.of(instanceData));
         }
@@ -64,13 +92,17 @@ class KdTreeParameterizedTest {
             return newArray;
         }
     }
-
     @DisplayName("should create a rectangle with the given coordinates and test KdTree's range() function")
-    @ParameterizedTest(name = "{index}=> kt={0},r={1},expectedPoints={2}")
+    @ParameterizedTest(name = "{index}=> file: {0}, rectangle ={2}, expectedPoints = {3}")
     @ArgumentsSource(KdTreeArgumentsProvider.class)
-    void range(KdTree kt, RectHV r, Point2D[] expectedPoints) {
-        System.out.println("running the test for tree size of: "+kt.size());
-        //Assertions.assertNotNull(kt.range(r));
-        Assertions.assertEquals(kt.size(),100);
+    void range(String filename, KdTree kdtree, RectHV rect, Point2D[] expectedPoints) throws IOException {
+        HashMap<Integer, Point2D> actualRange = new HashMap<>();
+        int i = 0;
+        for (Point2D p : kdtree.range(rect)) {
+            actualRange.put(i++, p);
+        }
+        for (Point2D p : expectedPoints) {
+            if (p != null) Assertions.assertTrue(actualRange.containsValue(p) && i == actualRange.size());
+        }
     }
 }
